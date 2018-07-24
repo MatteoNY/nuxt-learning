efc515c0ce22404650e577d1f56c5ac31f829c9e
Matt C <Matt@journeysoft.com>
2018-07-24 18:32:46 -0400

    Init
    Nuxt Learning

---
+++ components/Admin/AdminPostForm.vue
@@ -0,0 +1,58 @@
+<template>
+    <form @submit.prevent="onSave">
+    
+        <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
+    
+        <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+    
+        <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+    
+        <AppControlInput control-type="textarea" v-model="editedPost.content">Content</AppControlInput>
+    
+        <AppButton type="submit">Save</AppButton>
+    
+        <AppButton type="button" style="margin-left: 10px" btn-style="cancel" @click="onCancel">Cancel</AppButton>
+    
+    </form>
+</template>
+
+<script>
+    import AppControlInput from '@/components/UI/AppControlInput'
+    import AppButton from '@/components/UI/AppButton'
+    
+    export default {
+        props: {
+            post: {
+                types: Object,
+                required: false
+            }
+    
+        },
+        data() {
+            return {
+                editedPost: this.post
+                 ?  { ...this.post } 
+                 :  {
+                        author: '',
+                        title: '',
+                        thumnailLink: '',
+                        content: ''
+                    }
+            }
+        },
+        components: {
+            AppControlInput,
+            AppButton
+        },
+        methods: {
+            onSave() {
+                // save
+                console.log('save', this.editedPost)
+            },
+            onCancel() {
+                // navigate back
+                this.$router.push('/admin')
+            }
+        }
+    }
+</script>
---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,82 @@
+<template>
+<div class="single-posts-page">
+<section class="post">
+<h1>Posts details...</h1>
+<div class="post-details">
+    <div class="post-details">Last updated by xxx</div>
+    <div class="post-details">Written by Name</div>
+</div>
+<p class="post-content">Content...</p>
+</section>
+
+<section class="post-feedback">
+    <p>Please let me know what you think email me at: bla@bla.com <a href="mailto:Matt@journeysoft.com">M.C </a></p>
+
+</section>
+
+
+</div>
+</template>
+
+<style scoped>
+
+.single-post-page {
+  padding: 30px;
+  text-align: center;
+  box-sizing: border-box;
+}
+
+.post {
+  width: 100%;
+}
+
+@media (min-width: 768px) {
+  .post {
+    width: 600px;
+    margin: auto;
+  }
+}
+
+.post-title {
+  margin: 0;
+}
+
+.post-details {
+  padding: 10px;
+  box-sizing: border-box;
+  border-bottom: 3px solid #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+@media (min-width: 768px) {
+  .post-details {
+    flex-direction: row;
+  }
+}
+
+.post-detail {
+  color: rgb(88, 88, 88);
+  margin: 0 10px;
+}
+
+.post-feedback {
+     text-align: center;
+}
+
+.post-feedback a {
+  color: red;
+  text-decoration: none;
+ 
+}
+
+.post-feedback a:hover,
+.post-feedback a:active {
+  color: salmon;
+}
+
+
+
+</style>
